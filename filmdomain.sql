f09b67cd66c73910fb850a5860763d735bfc056a
maximilip <max.prasad@kellogg.northwestern.edu>
2026-01-28 18:24:49 +0000

    actors-table

--- filmdomain.sql
+++ filmdomain.sql
@@ -4,9 +4,9 @@
 
 -- Drop existing tables, so you'll start fresh each time this script is run.
 DROP TABLE IF EXISTS movies;
-DROP TABLE IF EXISTS agents;
 DROP TABLE IF EXISTS actors;
 DROP TABLE IF EXISTS performances;
+DROP TABLE IF EXISTS agents;
 
 -- Create new tables, according to your domain model
 CREATE TABLE movies (
@@ -17,23 +17,22 @@ CREATE TABLE movies (
   studio TEXT
 );
 
-CREATE TABLE agents (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  first_name TEXT,
-  last_name TEXT
-);
-
 CREATE TABLE actors (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  first_name TEXT,
-  last_name TEXT,
-  agent_id TEXT
+  name TEXT
 );
 
 CREATE TABLE performances (
   id INTEGER PRIMARY KEY AUTOINCREMENT,
-  movie_id TEXT,
-  actor_id TEXT
+  role TEXT,
+  movie_id INTEGER,
+  actor_id INTEGER
+);
+
+CREATE TABLE agents (
+  id INTEGER PRIMARY KEY AUTOINCREMENT,
+  first_name TEXT,
+  last_name TEXT
 );
 
 -- Insert data into your database that reflects the sample data shown above
@@ -74,10 +73,109 @@ VALUES (
     "PG-13",
     "Warner Bros."
 );
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Christian Bale"
+);INSERT INTO actors (
+  name
+)
+VALUES (
+    "Michael Caine"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Liam Neeson"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Katie Holmes"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Gary Oldman"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Aaron Eckhart"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Maggie Gyllenhaal"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Tom Hardy"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Joseph Gordon-Levitt"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Anne Hathaway"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Christian Bale"
+);INSERT INTO actors (
+  name
+)
+VALUES (
+    "Michael Caine"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Liam Neeson"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Katie Holmes"
+);
+INSERT INTO actors (
+  name
+)
+VALUES (
+    "Gary Oldman"
+);
+
 -- Prints a header for the movies output
 .print "Movies"
 .print "======"
 .print ""
 
 -- The SQL statement for the movies output goes here.
-SELECT title, year, rating, studio FROM movies;   
+SELECT * FROM movies;   
+
+-- Prints a header for the cast output
+.print ""
+.print "Top Cast"
+.print "========"
+.print ""
+
+-- The SQL statement for the cast output goes here.
+
+SELECT * FROM actors;
